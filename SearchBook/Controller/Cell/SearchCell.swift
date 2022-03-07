164987deb9e573022ecee31cc8fba19a2701e582
sedenkovvlad <vladsedenkov@gmail.com>
2022-03-07 16:55:58 +0300

    Add SDWebImage Framework and implemented networking

--- SearchBook/Controller/Cell/SearchCell.swift
+++ SearchBook/Controller/Cell/SearchCell.swift
@@ -7,13 +7,16 @@
 
 import Foundation
 import UIKit
+import SDWebImage
 
 final class SearchCell: UITableViewCell {
     static let reuseId = "SearchCell"
     
     private let bookImage: UIImageView = {
         let image = UIImageView()
-        image.backgroundColor = .red
+        image.layer.borderWidth = 1
+        image.layer.borderColor = #colorLiteral(red: 0.2352941176, green: 0.5098039216, blue: 0.5098039216, alpha: 1)
+        image.clipsToBounds = true
         return image
     }()
     
@@ -21,6 +24,7 @@ final class SearchCell: UITableViewCell {
        let label = UILabel()
         label.font = UIFont.boldSystemFont(ofSize: 15)
         label.numberOfLines = 0
+        label.textColor = #colorLiteral(red: 0.2352941176, green: 0.5098039216, blue: 0.5098039216, alpha: 1)
         return label
     }()
     
@@ -39,6 +43,14 @@ final class SearchCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func configureCell(from book: VolumeInfo?) {
+        titleLabel.text = book?.title
+        authorLabel.text = book?.authors?.joined(separator: ",")
+        guard let bookUrl = book?.imageLinks?.smallThumbnail else { return }
+        guard let url = URL(string: bookUrl) else { return }
+        bookImage.sd_setImage(with: url, completed: nil)
+    }
 }
 
 //MARK: UI
@@ -55,9 +67,8 @@ extension SearchCell {
         
         
         NSLayoutConstraint.activate([
-            bookImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            bookImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             bookImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            bookImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
             bookImage.heightAnchor.constraint(equalToConstant: 60),
             bookImage.widthAnchor.constraint(equalToConstant: 60)
         ])
@@ -72,7 +83,7 @@ extension SearchCell {
             authorLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
             authorLabel.leadingAnchor.constraint(equalTo: bookImage.trailingAnchor, constant: 8),
             authorLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            authorLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            authorLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
         ])
     }
 }
--- SearchBook/Controller/SearchViewController.swift
+++ SearchBook/Controller/SearchViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    private let fetcher: DataFetcher = NetworkDataFetcher(networking: NetworkService())
     private let tableView = UITableView()
+    private var books = [Items]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        fetchBook()
     }
 }
+
 
 //MARK: UITableViewDataSource
 extension SearchViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return books.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: SearchCell.reuseId, for: indexPath) as! SearchCell
+        let book = books[indexPath.row].volumeInfo
+        cell.backgroundColor = .clear
+        cell.configureCell(from: book)
         return cell
     }
 }
@@ -32,10 +39,25 @@ extension SearchViewController: UITableViewDelegate {
     
 }
 
+extension SearchViewController {
+    private func fetchBook() {
+        fetcher.fetchBook(search: "programming") { [weak self] books in
+            guard let self = self else { return }
+            guard let books = books?.items else { return }
+            self.books = books
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        
+    }
+}
+
 //MARK: - UI
 extension SearchViewController {
     private func configureTableView() {
         view.addSubview(tableView)
+        tableView.backgroundColor = .black
         tableView.frame = view.bounds
         tableView.delegate = self
         tableView.dataSource = self
